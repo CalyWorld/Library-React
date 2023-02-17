63857df25f1fb112e5b92c0d619eaeefd10c9890
Cal <obinnaanizoba5@gmail.com>
2023-02-17 11:44:57 +0800

    Organized the structure of the layout and passed bookCollection and handleDelete props

--- src/components/Main/main.jsx
+++ src/components/Main/main.jsx
@@ -1,11 +1,18 @@
 import React from "react";
 
-export const Main = ({ bookCollection }) => {
+export const Main = ({ bookCollection, handleDelete }) => {
   return (
-    <div className="m-0 p-0">
+    <div className="m-0 p-0 flex flex-row gap-10 justify-center">
       {bookCollection.map((book) => (
-        <div key={book.id}>
-          <div>{book.title}</div>
+        <div key={book.id} className="border-2 border-black">
+          <div>
+            <div>Title of Book : {book.title}</div>
+            <div>Name of Author: {book.author}</div>
+            <div>Page Number : {book.number}</div>
+          </div>
+          <div className="flex justify-end">
+            <button type="button" onClick={()=>{handleDelete(book)}}>Delete button</button>
+          </div>
         </div>
       ))}
     </div>
