b60ac6d87e1208d183e409211f5f6a01b6c7168d
Cal <obinnaanizoba5@gmail.com>
2023-02-17 23:01:38 +0800

    Added line through text decoration to mark book complete when the check mark has been clicked

--- src/components/Main/main.jsx
+++ src/components/Main/main.jsx
@@ -1,17 +1,18 @@
 import React from "react";
 
-export const Main = ({ bookCollection, handleDelete }) => {
+export const Main = ({ bookCollection, handleDelete, handleComplete }) => {
   return (
     <div className="m-0 p-0 flex flex-row gap-10 justify-center">
       {bookCollection.map((book) => (
         <div key={book.id} className="border-2 border-black">
           <div>
-            <div>Title of Book : {book.title}</div>
-            <div>Name of Author: {book.author}</div>
-            <div>Page Number : {book.number}</div>
+            <div><p className={`${book.completed ? "line-through" : ""}`}  >Title of Book : {book.title}</p></div>
+            <div><p className={`${book.completed ? "line-through" : ""}`}>Name of Author: {book.author}</p></div>
+            <div><p className={`${book.completed ? "line-through" : ""}`}>Page Number : {book.number}</p></div>
           </div>
-          <div className="flex justify-end">
-            <button type="button" onClick={()=>{handleDelete(book)}}>Delete button</button>
+          <div className="flex justify-end gap-4">
+            <button type="button" className="button-complete task-button" onClick={()=>{handleComplete(book)}}><i className="fa fa-check-circle"></i></button>
+            <button type="button" className="button-delete task-button" onClick={()=>{handleDelete(book)}}><i className="fa-solid fa-trash"></i></button>
           </div>
         </div>
       ))}
